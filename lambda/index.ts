072087a26ba36a53cebc7f1ebbbeca2bab473a9f
Xavier Riu <xavier.riu@launchmetrics.com>
2018-03-11 20:39:13 +0100

    Garbage commit

--- lambda/index.ts
+++ lambda/index.ts
@@ -2,6 +2,27 @@ import { createServer, proxy } from 'aws-serverless-express';
 import { app } from '../server/server';
 
 const awsServerlessExpress = require('aws-serverless-express');
+
+const binaryMimeTypes = [
+    'application/javascript',
+    'application/json',
+    'application/octet-stream',
+    'application/xml',
+    'image/jpeg',
+    'image/png',
+    'image/gif',
+    'text/comma-separated-values',
+    'text/css',
+    'text/html',
+    'text/javascript',
+    'text/plain',
+    'text/text',
+    'text/xml',
+    'image/x-icon',
+    'image/svg+xml',
+    'application/x-font-ttf'
+];
+
 const server = awsServerlessExpress.createServer(app);
 
 exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context);
--- src/main.ts
+++ src/main.ts
@@ -6,22 +6,22 @@ import { environment } from './environments/environment';
 
 if (environment.production) enableProdMode();
 
-// function registerServiceWorker(swName: string) {
-//   if ('serviceWorker' in navigator) {
-//     navigator.serviceWorker
-//       .register(`/${environment.stage}/${swName}.js`)
-//       .then(reg => {
-//         console.log('Successful service worker registration', reg);
-//       })
-//       .catch(err =>
-//         console.error('Service worker registration failed', err)
-//       );
-//   } else {
-//     console.error('Service Worker API is not supported in current browser');
-//   }
-// }
+function registerServiceWorker(swName: string) {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register(`/${swName}.js`)
+      .then(reg => {
+        console.log('Successful service worker registration', reg);
+      })
+      .catch(err =>
+        console.error('Service worker registration failed', err)
+      );
+  } else {
+    console.error('Service Worker API is not supported in current browser');
+  }
+}
 
-// registerServiceWorker('service-worker')
+// 
 document.addEventListener('DOMContentLoaded', () => {
   platformBrowserDynamic().bootstrapModule(AppModule, { preserveWhitespaces: false })
     .then(() => { })
